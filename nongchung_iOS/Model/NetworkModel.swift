5372e1511f43b8f2d3dcccc792f71ab84160087b
갓거 <taylored@naver.com>
2018-07-01 11:09:37 +0900

    start project

---
+++ nongchung_iOS/Model/NetworkModel.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkModel.swift
+//  nongchung_iOS
+//
+//  Created by 갓거 on 2018. 7. 1..
+//  Copyright © 2018년 농활청춘. All rights reserved.
+//
+
+class NetworkModel {
+
+    var view : NetworkCallback
+    
+    init(_ vc : NetworkCallback){
+        self.view = vc
+    }
+    
+    let baseURL = ""
+}
